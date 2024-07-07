f6bf223c3cc1d9a2e4de5fa0c79d1a22d0c3a61a
Akki-art-byte <akash1996srivastav@gmail.com>
2024-07-07 11:19:55 +0530

    Add files via upload

---
+++ DuplicateRemove.java
@@ -0,0 +1,18 @@
+package java8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DuplicateRemove {
+
+	public static void main(String[] args) {
+		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 6, 7, 8, 9, 0);
+
+		List<Integer> duplicateRemove = list.stream().distinct()
+				.collect(Collectors.toList());
+
+		System.out.println(duplicateRemove);
+	}
+
+}
